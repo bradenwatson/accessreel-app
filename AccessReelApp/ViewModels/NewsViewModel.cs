1fc8dcffcadef377d63f0181a17bcff24b1160d7
PlayVoidz510 <crazylol457@gmail.com>
2023-10-31 23:09:49 +0800

    Sample Carousel for Dylan to work with and any others who need it

--- AccessReelApp/ViewModels/NewsViewModel.cs
+++ AccessReelApp/ViewModels/NewsViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,5 +11,14 @@ namespace AccessReelApp.ViewModels
     public partial class NewsViewModel : ObservableObject
     {
         // Implement view model properties and funcs
+
+        [ObservableProperty]
+        ObservableCollection<string> myList = new()
+        {
+            "Item A",
+            "Item B",
+            "Item C",
+            "Item D",
+        };
     }
 }
