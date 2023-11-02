b28ce0d0e3a0613cc39162db8c1a3d57c67248bd
lilifi81 <lilifi8109@gmail.com>
2023-11-02 11:11:04 +0800

    Added filters to review page
    Filters for review page added

--- AccessReelApp/ViewModels/ReviewsViewModel.cs
+++ AccessReelApp/ViewModels/ReviewsViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,5 +11,29 @@ namespace AccessReelApp.ViewModels
     public partial class ReviewsViewModel : ObservableObject
     {
         // Implement view model properties and funcs
+        [ObservableProperty]
+        ObservableCollection<string> filterList = new()
+        {
+            "Newest",
+            "Oldest",
+            "Title (A-Z)",
+            "Title (Z-A)",
+            "Most Comments",
+            "Most Views",
+            "Most Followers",
+            "Top Site Rated",
+            "Top User Rated",
+            "Awards",
+        };
+
+        [ObservableProperty]
+        ObservableCollection<string> dateFilterList = new()
+        {
+            "Posted any date",
+            "Posted in the last year",
+            "Posted in the last month",
+            "Posted in the last week",
+            "Posted in the last day",
+        };
     }
 }
