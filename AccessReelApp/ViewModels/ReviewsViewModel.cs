565baef2137235b1592ccf7147d21f494bd7b47e
lilifi81 <lilifi8109@gmail.com>
2023-11-03 10:06:48 +0800

    Updated review page

--- AccessReelApp/ViewModels/ReviewsViewModel.cs
+++ AccessReelApp/ViewModels/ReviewsViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,5 +11,41 @@ namespace AccessReelApp.ViewModels
     public partial class ReviewsViewModel : ObservableObject
     {
         // Implement view model properties and funcs
+
+        [ObservableProperty]
+        ObservableCollection<string> filterList = new()
+        {
+            "Newest",
+            "Oldest",
+            "Title (A-Z)",
+            "Title (Z-A)",
+            "Most Comments",
+            "Most Views",
+            "Most Followers",
+            "Top Site Rated",
+            "Top User Rated",
+            "Awards",
+        };
+
+        [ObservableProperty]
+        ObservableCollection<string> dateFilterList = new()
+        {
+            "Posted any date",
+            "Posted in the last year",
+            "Posted in the last month",
+            "Posted in the last week",
+            "Posted in the last day",
+        };
+
+        [ObservableProperty]
+        ObservableCollection<ReviewCell> movieReviewsList = new()
+        {
+            new ReviewCell
+            {
+                MovieTitle = "Teenage Mutant Ninja Turtles: Mutant Mayhem Review",
+                MovieDescription = "Paramount Pictures and Nickelodeon Films presents their most radical movie this year - Teenage Mutant Ninja Turtles: Mutant Mayhem.",
+                PosterUrl = "Images/Icons/movies/turtles.jpg",
+            }
+        };
     }
 }
