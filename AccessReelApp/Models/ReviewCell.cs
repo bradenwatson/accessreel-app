7161109a38b0d3f17378700b217632394e2e4dd5
George <20092133@tafe.wa.edu.au>
2023-11-09 11:46:33 +0800

    Revert "Revert "Merge branch 'main' into database-team""
    This reverts commit 3a7d4deae35090d9002e6cc47c3d191bb8a71477.

---
+++ AccessReelApp/Models/ReviewCell.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessReelApp
+{
+    public class ReviewCell
+    {
+        public string MovieTitle { get; set; }
+        public string MovieDescription { get; set; }
+        public string PosterUrl { get; set; }
+        public string CriticUrl { get; set; }
+        public float MovieRating { get; set; }
+    }
+}
