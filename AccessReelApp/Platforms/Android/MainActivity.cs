197595584cd23870513583abd21a715cfe74d747
Tony Bui <140578297+tonyb-99@users.noreply.github.com>
2023-11-20 02:06:56 +0800

    Created new channel

--- AccessReelApp/Platforms/Android/MainActivity.cs
+++ AccessReelApp/Platforms/Android/MainActivity.cs
@@ -7,7 +7,7 @@ namespace AccessReelApp;
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
-    internal static readonly string Channel_ID = "TestChannel";
+    internal static readonly string Channel_ID = "NewChannel";
     internal static readonly int NotificationID = 0; 
     protected override void OnCreate(Bundle savedInstanceState)
     {
@@ -31,11 +31,11 @@ public class MainActivity : MauiAppCompatActivity
         CreateNotificationChannel();
     }
 
-    private void CreateNotificationChannel()
+    private void CreateNotificationChannel()        //Need to create first on app start, not on button press. This is due to android 8
     {
         if (OperatingSystem.IsOSPlatformVersionAtLeast("android", 26))
         {
-            var channel = new NotificationChannel(Channel_ID, "Test Notification Channel", NotificationImportance.Default);
+            var channel = new NotificationChannel(Channel_ID, "New Notification Channel", NotificationImportance.Default);
             var notificationManager = (NotificationManager)GetSystemService(Android.Content.Context.NotificationService);
             notificationManager.CreateNotificationChannel(channel); 
         }
