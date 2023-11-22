6e92914041000387c75ffd9c474e6fb4a263522a
GregusRich <141307499+GregusRich@users.noreply.github.com>
2023-11-23 00:54:30 +0800

    Attempted GPS with map display using Maui.Maps, Andriod uses Google Maps which needs an API so I was unsuccessful. Until either API obtained or another solution is found we may need to use an image which would represent the functionality.

--- AccessReelApp/ViewModels/MapsPageViewModel.cs
+++ AccessReelApp/ViewModels/MapsPageViewModel.cs
@@ -1,15 +1,27 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.Maui.Maps;
+using Microsoft.Maui.Controls.Maps;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Plugin.LocalNotification.NotificationRequestGeofence;
 
 namespace AccessReelApp.ViewModels
 {
     public partial class MapsPageViewModel : ObservableObject
     {
-        [ObservableProperty] string example = "heyaaaaa greg!! XDDDD";
-        // stuff here
+        // Define an initial location for the map
+        [ObservableProperty]
+        public MapSpan initialMapLocation;
+
+        public MapsPageViewModel()
+        {
+            // Set an initial location for the map when the ViewModel is created
+            InitialMapLocation = MapSpan.FromCenterAndRadius(
+                new Location(-33.8688, 151.2093), 
+                Distance.FromKilometers(50));    // Zoom level 
+        }
     }
 }
