c2af385ef09de33ce5527d0a13c71a1e8319707b
L <110619033+Learner062022@users.noreply.github.com>
2023-11-24 15:15:10 +0800

    Changed functions' names.

--- AccessReelApp/Models/CarouselModel.cs
+++ AccessReelApp/Models/CarouselModel.cs
@@ -25,24 +25,35 @@ namespace AccessReelApp
         // Need to get position image button and set indicator view index to that
 
         // Initialises the dictionaries and image buttons
-        static void Initialise()
+        static void InitialiseDictionariesAndImageButtons()
         {
-            List<string> keys = ListKeys();
+            List<string> keys = ListDictionaryKeys();
             PopulateDictionaries(keys, TRAILER, trailersDictionary, trailerImageButtons);
             PopulateDictionaries(keys, string.Empty, otherDictionary, otherImageButtons);
         }
 
+        static bool IsTrailerInTitle(string title)
+        {
+            return title.Contains(TRAILER);
+        }
+
         // Gets a list of keys from the movieImageDictionary
-        static List<string> ListKeys()
+        static List<string> ListDictionaryKeys()
         {
             return new List<string>(movieImageDictionary.Keys);
+        }
+
+        // Finds keys in the list containing the specified key and returns a sublist.
+        static List<string> FindKeysContainingSubstring(string key, List<string> keys)
+        {
+            return keys.FindAll(item => item.Contains(key)).Take(NUMBER_MOVIES).ToList();
         }
 
         // Populates dictionaries and ImageButtons based on a filter (TRAILER or other)
         static void PopulateDictionaries(List<string> keys, string filter, Dictionary<string, string>[] dictionaries, ImageButton[] buttons)
         {
             // Selects keys based on the filter
-            List<string> selectedKeys = (filter == TRAILER) ? FindKeys(TRAILER, keys) : keys.Except(FindKeys(TRAILER, keys)).Take(NUMBER_MOVIES).ToList();
+            List<string> selectedKeys = (filter == TRAILER) ? FindKeysContainingSubstring(TRAILER, keys) : keys.Except(FindKeysContainingSubstring(TRAILER, keys)).Take(NUMBER_MOVIES).ToList();
 
             // Populates dictionaries and image buttons based on selected keys
             for (int i = 0; i < NUMBER_MOVIES; i++)
@@ -54,16 +65,10 @@ namespace AccessReelApp
             }
         }
 
-        // Finds keys in the list containing the specified key and returns a sublist.
-        static List<string> FindKeys(string key, List<string> keys)
-        {
-            return keys.FindAll(item => item.Contains(key)).Take(NUMBER_MOVIES).ToList();
-        }
-
         // Creates an array of dictionaries for a filtered subset of keys.
         static Dictionary<string, string>[] CreateDictionaryArray(List<string> keys, string filter)
         {
-            return FindKeys(filter, keys).Select(key => CreateDictionary(key, movieImageDictionary[key])).ToArray();
+            return FindKeysContainingSubstring(filter, keys).Select(key => CreateDictionary(key, movieImageDictionary[key])).ToArray();
         }
 
         // Creates an array of image buttons based on the list's elements.
@@ -73,18 +78,18 @@ namespace AccessReelApp
         }
 
         // Returns an array of dictionaries based on the parameter's value.
-        static Dictionary<string, string>[] GetSelectedDict(string filter)
+        static Dictionary<string, string>[] GetArrayDictionaries(string title)
         {
-            return filter == TRAILER ? trailersDictionary : otherDictionary;
+            return IsTrailerInTitle(title) ? trailersDictionary : otherDictionary;
         }
 
         // Returns an array of image buttons based on the parameter's value.
-        static ImageButton[] GetSelectedImageButtons(string filter)
+        static ImageButton[] GetSelectedImageButtons(string title)
         {
-            return filter == TRAILER ? trailerImageButtons : otherImageButtons;
+            return IsTrailerInTitle(title) ? trailerImageButtons : otherImageButtons;
         }
 
-        public static void AddMovieToDictionaries(ReviewCell reviewCell, int index)
+        public static void UpdateDictionariesAndArrayImageButtons(ReviewCell reviewCell, int index)
         {
             // Concatenates "Movie_" with the value of variable "index" 
             string key = $"Movie_{index}";
@@ -93,7 +98,7 @@ namespace AccessReelApp
             movieImageDictionary[key] = reviewCell.PosterUrl;
 
             // Gets the selected dictionaries and ImageButtons based on the reviewCell.MovieTitle
-            Dictionary<string, string>[] selectedDictionary = GetSelectedDict(reviewCell.MovieTitle);
+            Dictionary<string, string>[] selectedDictionary = GetArrayDictionaries(reviewCell.MovieTitle);
             selectedDictionary[index] = CreateDictionary(reviewCell.MovieTitle, reviewCell.PosterUrl);
 
             GetSelectedImageButtons(reviewCell.MovieTitle)[index] = CreateImageButton(reviewCell.PosterUrl);
