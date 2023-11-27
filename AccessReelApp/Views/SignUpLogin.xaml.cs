27d40e40e5cee1c66d39c1caa352781c204eacfd
Braden Watson <brwatson10@gmail.com>
2023-11-27 18:09:42 +0800

    Add URLs to Facebook and Google icons, fix label navigation on login page

--- AccessReelApp/Views/SignUpLogin.xaml.cs
+++ AccessReelApp/Views/SignUpLogin.xaml.cs
@@ -21,4 +21,44 @@ public partial class SignUpLogin : ContentPage, IRecipient<OpenPageMessage>
             await Shell.Current.DisplayAlert(message.Value, "Please enter username and password", "OK");
         }
     }
+
+    private async void FacebookImageButton_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            Uri uri = new Uri(
+                "https://accessreel.com/wp/wp-login.php?action=wordpress_social_authenticate&mode=login&provider=Facebook&redirect_to=https%3A%2F%2Faccessreel.com%2Fwp%2Fwp-login.php");
+
+            BrowserLaunchOptions options = new BrowserLaunchOptions()
+            {
+                LaunchMode = BrowserLaunchMode.SystemPreferred,
+                TitleMode = BrowserTitleMode.Show,
+                PreferredToolbarColor = Colors.Violet,
+                PreferredControlColor = Colors.SandyBrown
+            };
+
+            await Browser.Default.OpenAsync(uri, options);
+        }
+        catch { return; }
+    }
+
+    private async void GoogleImageButton_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            Uri uri = new Uri(
+                "https://accessreel.com/wp/wp-login.php?action=wordpress_social_authenticate&mode=login&provider=Google&redirect_to=https%3A%2F%2Faccessreel.com%2Fwp%2Fwp-login.php");
+
+            BrowserLaunchOptions options = new BrowserLaunchOptions()
+            {
+                LaunchMode = BrowserLaunchMode.SystemPreferred,
+                TitleMode = BrowserTitleMode.Show,
+                PreferredToolbarColor = Colors.Violet,
+                PreferredControlColor = Colors.SandyBrown
+            };
+
+            await Browser.Default.OpenAsync(uri, options);
+        }
+        catch { return; }
+    }
 }
